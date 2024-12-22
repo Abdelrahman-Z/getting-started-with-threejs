877678d62dfeb93e093a6a208c73aeec14b81b84
Abdelrahman Zidan <abdelrahmanziedan@gmail.com>
2024-12-22 18:29:39 +0200

    new scene

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,21 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import * as dat from "dat.gui";
-import nebula from "../images/nebula.jpg";
-import stars from "../images/stars.jpg";
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+
+import starsTexture from '../images/stars.jpg';
+import sunTexture from '../images/sun.jpg';
+import mercuryTexture from '../images/mercury.jpg';
+import venusTexture from '../images/venus.jpg';
+import earthTexture from '../images/earth.jpg';
+import marsTexture from '../images/mars.jpg';
+import jupiterTexture from '../images/jupiter.jpg';
+import saturnTexture from '../images/saturn.jpg';
+import saturnRingTexture from '../images/saturn ring.png';
+import uranusTexture from '../images/uranus.jpg';
+import uranusRingTexture from '../images/uranus ring.png';
+import neptuneTexture from '../images/neptune.jpg';
+import plutoTexture from '../images/pluto.jpg';
 
 const renderer = new THREE.WebGLRenderer();
-renderer.shadowMap.enabled = true;
-// renderer.setClearColor(0xffea00);
-const textureLoader = new THREE.TextureLoader();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -15,187 +23,120 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-// scene.background = textureLoader.load(stars);
-const cubeTexture = new THREE.CubeTextureLoader().load([
-  stars,
-  stars,
-  stars,
-  stars,
-  stars,
-  stars,
-]);
-
-scene.background = cubeTexture;
-
 const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
-
-const axesHelper = new THREE.AxesHelper(5);
-scene.add(axesHelper);
-
-// const gridHelper = new THREE.GridHelper(100, 100);
-// scene.add(gridHelper);
-
-const BoxGeometry2 = new THREE.BoxGeometry(4, 4, 4);
-// const material2 = new THREE.MeshBasicMaterial({ color: 0x00ff00  , map: textureLoader.load(nebula) });
-const material2 = [
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(nebula) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(nebula) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(stars) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(nebula) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(stars) }),
-  new THREE.MeshBasicMaterial({ map: textureLoader.load(nebula) }),
-];
-const box2 = new THREE.Mesh(BoxGeometry2, material2);
-box2.position.set(0, 15, 10);
-box2.name = "nebula";
-scene.add(box2);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
-const box = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(box, material);
-scene.add(cube);
-
-const planeGemoetry = new THREE.PlaneGeometry(60, 60);
-const planeMaterial = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  side: THREE.DoubleSide,
-});
-const plane = new THREE.Mesh(planeGemoetry, planeMaterial);
-plane.rotation.x = -0.5 * Math.PI;
-plane.receiveShadow = true;
-scene.add(plane);
-
-const plane2Gemoetry = new THREE.PlaneGeometry(10 , 10 , 10 , 10);
-const plane2Material = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  wireframe: true,
-})
-const plane2 = new THREE.Mesh(plane2Gemoetry, plane2Material);
-scene.add(plane2);
-plane2.position.set(10, 10, 15);
-
-plane2.geometry.attributes.position.array[0] -=10 * Math.random()
-plane2.geometry.attributes.position.array[1] -=10 * Math.random()
-plane2.geometry.attributes.position.array[2] -=10 * Math.random()
-plane2.geometry.attributes.position.array.at(-1) = 10 * Math.random()
-// plane2.geometry.attributes.position.array[3] = -10 * Math.random()
-
-
-const sphereGeometry = new THREE.SphereGeometry(5, 50, 50);
-const sphereMaterial = new THREE.MeshStandardMaterial({
-  color: 0xff0000,
-  wireframe: false,
-});
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.position.set(-10, 10, 0);
-sphere.castShadow = true;
-scene.add(sphere);
-
-const sphereId = sphere.uuid;
-
-camera.position.set(-10, 30, 30);
+camera.position.set(-90, 140, 140);
 orbit.update();
 
-const ambitLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambitLight);
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
 
-// const directionalLight = new THREE.DirectionalLight(0xffffff, 30);
-// directionalLight.position.set(15, 20, -20);
-// directionalLight.castShadow = true;
-// directionalLight.shadow.camera.right = 12;
-// directionalLight.shadow.camera.top = 25;
-// scene.add(directionalLight);
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+scene.background = cubeTextureLoader.load([
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture,
+    starsTexture
+]);
 
-// const dLightHelper = new THREE.DirectionalLightHelper(directionalLight , 1);
-// scene.add(dLightHelper);
+const textureLoader = new THREE.TextureLoader();
 
-// const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
-// scene.add(dLightShadowHelper);
-
-const spotLight = new THREE.SpotLight(0xffffff, 100000);
-spotLight.position.set(-100, 100, 0);
-spotLight.castShadow = true;
-spotLight.angle = 0.1;
-scene.add(spotLight);
-
-const sLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(sLightHelper);
-
-const gui = new dat.GUI();
-
-const options = {
-  sphereColor: "#ffea00",
-  wireframe: false,
-  speed: 0.01,
-  XPosition: 0,
-  angel: 0.2,
-  penumbra: 0,
-  intensety: 1,
-};
-
-// scene.fog = new THREE.Fog(0x000000, 0, 200);
-// scene.fog = new THREE.FogExp2(0xffffff, 0.01);
-
-gui.addColor(options, "sphereColor").onChange((value) => {
-  sphere.material.color.set(value);
+const sunGeo = new THREE.SphereGeometry(16, 30, 30);
+const sunMat = new THREE.MeshBasicMaterial({
+    map: textureLoader.load(sunTexture)
 });
-gui.add(options, "wireframe").onChange((value) => {
-  sphere.material.wireframe = value;
-});
+const sun = new THREE.Mesh(sunGeo, sunMat);
+scene.add(sun);
 
-gui.add(options, "speed", 0, 0.1);
-gui.add(options, "angel", 0, 1);
-gui.add(options, "penumbra", 0, 1);
-gui.add(options, "intensety", 0, 100000);
-
-gui.add(options, "XPosition", -20, 20).onChange((value) => {
-  sphere.position.x = value;
-});
-
-let step = 0;
-
-const mousePosition = new THREE.Vector2();
-
-window.addEventListener("mousemove", (event) => {
-  mousePosition.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mousePosition.y = -(event.clientY / window.innerHeight) * 2 + 1;
-});
-
-const rayCaster = new THREE.Raycaster();
-
-function animate(time) {
-  cube.rotation.x = time / 1000;
-  cube.rotation.y = time / 1000;
-  cube.rotation.z = time / 1000;
-
-  step += options.speed;
-  sphere.position.y = 10 * Math.abs(Math.sin(step)) + 5;
-  spotLight.intensity = options.intensety;
-  spotLight.angle = options.angel;
-  spotLight.penumbra = options.penumbra;
-  sLightHelper.update();
-
-  rayCaster.setFromCamera(mousePosition, camera);
-  const intersects = rayCaster.intersectObjects(scene.children);
-  console.log(intersects);
-  for (let i = 0; i < intersects.length; i++) {
-    if (intersects[i].object.uuid === sphereId) {
-      intersects[i].object.material.color.set(0xff0000);
+function createPlanete(size, texture, position, ring) {
+    const geo = new THREE.SphereGeometry(size, 30, 30);
+    const mat = new THREE.MeshStandardMaterial({
+        map: textureLoader.load(texture)
+    });
+    const mesh = new THREE.Mesh(geo, mat);
+    const obj = new THREE.Object3D();
+    obj.add(mesh);
+    if(ring) {
+        const ringGeo = new THREE.RingGeometry(
+            ring.innerRadius,
+            ring.outerRadius,
+            32);
+        const ringMat = new THREE.MeshBasicMaterial({
+            map: textureLoader.load(ring.texture),
+            side: THREE.DoubleSide
+        });
+        const ringMesh = new THREE.Mesh(ringGeo, ringMat);
+        obj.add(ringMesh);
+        ringMesh.position.x = position;
+        ringMesh.rotation.x = -0.5 * Math.PI;
     }
-    if (intersects[i].object.name === "nebula") {
-      box2.rotation.x = time / 1000;
-      box2.rotation.y = time / 1000;
-      box2.rotation.z = time / 1000;
-    }
-  }
-  renderer.render(scene, camera);
+    scene.add(obj);
+    mesh.position.x = position;
+    return {mesh, obj}
+}
+
+const mercury = createPlanete(3.2, mercuryTexture, 28);
+const venus = createPlanete(5.8, venusTexture, 44);
+const earth = createPlanete(6, earthTexture, 62);
+const mars = createPlanete(4, marsTexture, 78);
+const jupiter = createPlanete(12, jupiterTexture, 100);
+const saturn = createPlanete(10, saturnTexture, 138, {
+    innerRadius: 10,
+    outerRadius: 20,
+    texture: saturnRingTexture
+});
+const uranus = createPlanete(7, uranusTexture, 176, {
+    innerRadius: 7,
+    outerRadius: 12,
+    texture: uranusRingTexture
+});
+const neptune = createPlanete(7, neptuneTexture, 200);
+const pluto = createPlanete(2.8, plutoTexture, 216);
+
+const pointLight = new THREE.PointLight(0xFFFFFF, 2, 300);
+scene.add(pointLight);
+
+
+function animate() {
+    //Self-rotation
+    sun.rotateY(0.004);
+    mercury.mesh.rotateY(0.004);
+    venus.mesh.rotateY(0.002);
+    earth.mesh.rotateY(0.02);
+    mars.mesh.rotateY(0.018);
+    jupiter.mesh.rotateY(0.04);
+    saturn.mesh.rotateY(0.038);
+    uranus.mesh.rotateY(0.03);
+    neptune.mesh.rotateY(0.032);
+    pluto.mesh.rotateY(0.008);
+
+    //Around-sun-rotation
+    mercury.obj.rotateY(0.04);
+    venus.obj.rotateY(0.015);
+    earth.obj.rotateY(0.01);
+    mars.obj.rotateY(0.008);
+    jupiter.obj.rotateY(0.002);
+    saturn.obj.rotateY(0.0009);
+    uranus.obj.rotateY(0.0004);
+    neptune.obj.rotateY(0.0001);
+    pluto.obj.rotateY(0.00007);
+
+    renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
